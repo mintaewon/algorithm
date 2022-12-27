ee07440d96af4a5b222491af8fbf4ebd516aff3e
Taewon Min <taewon1230@naver.com>
2022-12-27 14:15:54 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.py
@@ -0,0 +1,11 @@
+def solution(ingredient):
+    answer = 0
+    idx = 0
+    while idx < (len(ingredient)-3):
+        if ingredient[idx:idx+4] == [1,2,3,1]:
+            del ingredient[idx:idx+4]
+            answer += 1
+            idx = idx-3
+            continue
+        idx +=1
+    return answer
