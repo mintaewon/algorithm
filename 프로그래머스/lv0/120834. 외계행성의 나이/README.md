c4a931b856e0e1da1784e4dd30078e6581975ab8
Taewon Min <taewon1230@naver.com>
2022-12-29 20:08:37 +0900

    [level 0] Title: 외계행성의 나이, Time: 1.95 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120834. 외계행성의 나이/README.md
+++ 프로그래머스/lv0/120834. 외계행성의 나이/README.md
@@ -1,10 +1,10 @@
 # [level 0] 외계행성의 나이 - 120834 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120834) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120834?language=java) 
 
 ### 성능 요약
 
-메모리: 78.6 MB, 시간: 0.36 ms
+메모리: 76.7 MB, 시간: 1.95 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.java
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class Solution {
     public String solution(int age) {
-        // String answer = "";
+        String answer = "";
         HashMap<Integer, Character> dic = new HashMap<Integer, Character>();
         int num = 0;
         char a = 'a';
@@ -12,14 +12,23 @@ class Solution {
             num++;
         }
         
-        String st = Integer.toString(age);
-        String[] stls = st.split("");
-        StringBuffer ans = new StringBuffer();
-        for (String k : stls){
-            int tmp = Integer.valueOf(k).intValue();
-            ans.append(dic.get(tmp));
+        // String st = Integer.toString(age);
+        // String[] stls = st.split("");
+        // StringBuffer ans = new StringBuffer();
+        // for (String k : stls){
+        //     int tmp = Integer.valueOf(k).intValue();
+        //     ans.append(dic.get(tmp));
+        //     answer += dic.get(tmp);
+        // }
+        
+        String st_age = Integer.toString(age);
+        // System.out.println(st_age);
+        for (String k : st_age.split("")){
+            int j = Integer.valueOf(k).intValue();
+            answer += Character.toString(dic.get(j));
+            // System.out.println(dic.get(j));
         }
-        System.out.println(ans);
-        return ans.toString();
+        // return ans.toString();
+        return answer;
     }
 }
